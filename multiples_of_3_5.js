d698329396b45a98ab1eaaff41d92630836ce751
EdMagal <ed83isme@gmail.com>
2017-08-17 13:31:00 -0300

    Create multiples_of_3_5.js
    logs the answer requested into the console (press F12 in your browser).

---
+++ multiples_of_3_5.js
@@ -0,0 +1,4 @@
+console.log("There are " + var x = Math.floor(1000/3) +  " multiples of 3 between 1 and 1000.");
+console.log("There are " + var y = Math.floor(1000/5) +  " multiples of 5 between 1 and 1000.");
+console.log("There are " + var z = Math.floor(1000/15)+ " multiples of 15 between 1 and 1000.");
+console.log("Therefore, there is a total of " + x+y-z + " mutiple of 3 or 5 between 1 and 1000.");
